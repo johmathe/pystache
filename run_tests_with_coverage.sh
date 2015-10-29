050c7846e2fe721fa84ff63cb56cee51895256f2
Johan Mathe <johmathe@users.noreply.github.com>
2015-10-29 15:35:19 -0700

    Update run_tests_with_coverage.sh

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,13 +1,19 @@
-#!/usr/bin/env bash
-
-. ~/.virtualenvs/python2.7/bin/activate
+#!/bin/bash
 
 rm -f pep8.log pyflakes.log
 
-PYTHONPATH=. python -m coverage run test.py
-
+# Run python tests related to this project.
+export PYTHONPATH=${PYTHONPATH}:./python
+./test.py
+nosetests --with-xunit \
+          --all-modules \
+          --traverse-namespace \
+          --with-coverage \
+          --cover-package=generate_shards \
+          --cover-inclusive \
+          python/tests
+python -m coverage run test.py
 python -m coverage xml -o coverage.xml
 python -m coverage html -d coverage
-
-pep8 --max-line-length=120 pystache > pep8.log || true
-pyflakes pystache > pyflakes.log || true
+find . -iname "*.py"| xargs pylint > pylint.log || true
+find . -iname "*.py"| xargs pep8 > pep8.log || true
